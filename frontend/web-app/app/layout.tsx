59e99d3109c3531149b090d0ad533c9336126c31
richard augusto bueno <richard_bueno@ymail.com>
2023-09-26 22:14:08 -0300

    feat: frontend initial commit

---
+++ frontend/web-app/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from "next";
+import "./globals.css";
+import Navbar from "./nav/Navbar";
+
+export const metadata: Metadata = {
+  title: "Carsties",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="container mx-auto px-5 pt-10">{children}</main>
+      </body>
+    </html>
+  );
+}
