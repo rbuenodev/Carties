38d974d2c85c729a240ff0615327c4ee5fa9f27d
richard augusto bueno <richard_bueno@ymail.com>
2023-10-03 22:45:00 -0300

    feat: adding pagination

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,22 +1,56 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import AuctionCard from "./AuctionCard";
-import { PageResult, Auction } from "@/types";
+import AppPagination from "../components/AppPagination";
+import { Auction, PageResult } from "@/types";
+import { getData } from "../actions/auctionAction";
+import Filters from "./Filters";
+import { useParamsStore } from "@/hooks/useParamsStore";
+import { shallow } from "zustand/shallow";
+import qs from "query-string";
 
-async function getData(): Promise<PageResult<Auction>> {
-  const res = await fetch("http://localhost:6001/search?pageSize=10");
-  if (!res.ok) throw new Error("Failed to fetch data");
-  return res.json();
-}
+export default function Listings() {
+  const [data, setData] = useState<PageResult<Auction>>();
+  const params = useParamsStore(
+    (state) => ({
+      pageNumber: state.pageNumber,
+      pageSize: state.pageSize,
+      searchTerm: state.searchTerm,
+    }),
+    shallow
+  );
+  const setParams = useParamsStore((state) => state.setParams);
+  const url = qs.stringifyUrl({ url: "", query: params });
 
-export default async function Listings() {
-  const data = await getData();
+  function setPageNumber(pageNumber: number) {
+    setParams({ pageNumber });
+  }
+
+  useEffect(() => {
+    getData(url).then((data) => {
+      setData(data);
+    });
+  }, [url]);
+
+  if (!data) return <h3>Loading...</h3>;
 
   return (
-    <div className="grid grid-cols-4 gap-6">
-      {data &&
-        data.results.map((auction) => (
-          <AuctionCard key={auction.id} auction={auction} />
-        ))}
-    </div>
+    <>
+      <Filters />
+      <div className="grid grid-cols-4 gap-6">
+        {data.results &&
+          data.results.map((auction) => (
+            <AuctionCard key={auction.id} auction={auction} />
+          ))}
+      </div>
+      <div className="flex justify-center mt-4">
+        <AppPagination
+          currentPage={params.pageNumber}
+          pageCount={data.pageCount}
+          pageChange={setPageNumber}
+        />
+      </div>
+    </>
   );
 }
---
+++ frontend/web-app/hooks/useParamsStore.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+
+type State = {
+  pageNumber: number;
+  pageSize: number;
+  pageCount: number;
+  searchTerm: string;
+};
+
+type Actions = {
+  setParams: (params: Partial<State>) => void;
+  reset: () => void;
+};
+
+const initiaLState: State = {
+  pageNumber: 1,
+  pageSize: 12,
+  pageCount: 1,
+  searchTerm: "",
+};
+
+export const useParamsStore = create<State & Actions>()((set) => ({
+  ...initiaLState,
+  setParams: (newParams: Partial<State>) => {
+    set((state) => {
+      if (newParams.pageNumber) {
+        return { ...state, pageNumber: newParams.pageNumber };
+      } else {
+        return { ...state, ...newParams, pageNumber: 1 };
+      }
+    });
+  },
+  reset: () => set(initiaLState),
+}));
