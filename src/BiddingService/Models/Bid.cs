4f4731612aedde5bbeba6bab43c857502e24083c
richard augusto bueno <richard_bueno@ymail.com>
2023-10-25 22:34:54 -0300

    feat: adding bid service

---
+++ src/BiddingService/Models/Bid.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Entities;
+
+namespace BiddingService;
+
+public class Bid : Entity
+{
+    public string AuctionId { get; set; }
+    public string Bidder { get; set; }
+    public DateTime BidTime { get; set; }
+    public int Amount { get; set; }
+    public BidStatus BidStatus { get; set; }
+
+}
