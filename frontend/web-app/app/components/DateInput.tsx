f3d059ff90df9dbf6a6ad1207393eb71515998bb
richard augusto bueno <richard_bueno@ymail.com>
2024-02-07 22:30:20 -0300

    fix: adding https

--- frontend/web-app/app/components/DateInput.tsx
+++ frontend/web-app/app/components/DateInput.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { UseControllerProps, useController } from "react-hook-form";
-import "react-datepicker/dist/react-datepicker.css";
 import DatePicker, { ReactDatePickerProps } from "react-datepicker";
 
 type Props = {
